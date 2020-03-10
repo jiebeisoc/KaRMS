b5a43e32d64193509fb97f226960e3ba3f40efeb
jiebei <jiebei@comp.nus.edu.sg>
2020-03-10 12:15:06 +0800

    delete comment on exception

--- KaRMS-ejb/src/java/util/exception/CustomerNotFoundException.java
+++ KaRMS-ejb/src/java/util/exception/CustomerNotFoundException.java
@@ -11,19 +11,9 @@ package util.exception;
  */
 public class CustomerNotFoundException extends Exception {
 
-    /**
-     * Creates a new instance of <code>CustomerNotFoundException</code> without
-     * detail message.
-     */
     public CustomerNotFoundException() {
     }
 
-    /**
-     * Constructs an instance of <code>CustomerNotFoundException</code> with the
-     * specified detail message.
-     *
-     * @param msg the detail message.
-     */
     public CustomerNotFoundException(String msg) {
         super(msg);
     }
