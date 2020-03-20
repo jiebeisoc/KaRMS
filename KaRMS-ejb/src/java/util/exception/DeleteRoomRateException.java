3d727e08eeba7ffc0f879193eb82e7a2b9a8a4fa
jiebei <jiebei@comp.nus.edu.sg>
2020-03-20 20:15:56 +0800

    add room entity exception

---
+++ KaRMS-ejb/src/java/util/exception/DeleteRoomRateException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.exception;
+
+/**
+ *
+ * @author chai
+ */
+public class DeleteRoomRateException extends Exception {
+
+    public DeleteRoomRateException() {
+    }
+
+    public DeleteRoomRateException(String message) {
+        super(message);
+    }
+    
+}
